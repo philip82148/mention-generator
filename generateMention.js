43dacf977eeeedbbc306c04aa838d295b5898dc1
philip82148 <92205270+philip82148@users.noreply.github.com>
2023-05-06 19:57:16 +0900

    refactor generateMention

--- generateMention.js
+++ generateMention.js
@@ -1,5 +1,117 @@
 import { showBanner } from "./showBanner.js";
 
+export function generateMentions(members, pageSource, shorten) {
+  const submittedMembers = [];
+  const unSubmittedMembers = [];
+  const lineNames = [];
+  const regexps = [];
+  for (const member of members) {
+    const [resultMember, regexpsPerMember] = searchForMember(
+      member,
+      pageSource
+    );
+    if (resultMember.hitTerm) {
+      submittedMembers.push(resultMember);
+    } else {
+      unSubmittedMembers.push(resultMember);
+    }
+    lineNames.push(resultMember.lineName);
+    regexps.push(...regexpsPerMember);
+  }
+
+  let mentions = unSubmittedMembers.map((member) => "@" + member.lineName);
+  if (shorten) mentions = shortenMentions(mentions, lineNames);
+
+  return { mentions, submittedMembers, unSubmittedMembers, regexps };
+}
+
+function searchForMember(member, pageSource) {
+  const fullName = member[0].trim();
+  const lineName = unAt(member[member.length - 1].trim());
+
+  const regexps = [];
+  for (let i = 0; ; i++) {
+    // lineNameは検索しない。但し要素数が1個しかないときは別
+    if (i >= member.length - 1 && member.length > 1) break;
+
+    // 空白は空白を任意の数含む文字として検索
+    const searchKeyword = member[i].trim();
+    const regexp = new RegExp(
+      escapeRegexp(searchKeyword).replace(/[ 　]+/g, "( |　|&nbsp;)*"),
+      "ig"
+    );
+    regexps.push(regexp);
+
+    // 検索
+    if (pageSource.match(regexp))
+      return [{ fullName, lineName, hitKeyword: searchKeyword }, regexps];
+  }
+
+  return [{ fullName, lineName }, regexps];
+}
+
+function shortenMentions(mentions, lineNames) {
+  const loweredNames = lineNames.map((lineName) => lowerAll(lineName));
+  return mentions.map((mention) => {
+    const lineName = unAt(mention);
+    const loweredName = lowerAll(lineName);
+
+    const [shortestString, index] = getShortestUniqueString(
+      loweredName,
+      loweredNames
+    );
+    return "@" + lineName.substring(index, index + shortestString.length);
+  });
+}
+
+function getShortestUniqueString(string, strings) {
+  strings = strings.filter((from) => from !== string);
+
+  // length=1~string.length-1
+  // length=string.lengthはしない
+  for (let length = 1; length < string.length; length++) {
+    // index=0~string.length-length(lengthを引いて余ったスペースだけ動く)
+    for (let index = 0; index <= string.length - length; index++) {
+      const substring = string.substring(index, index + length);
+
+      // 検索するsubstringがスペースで囲まれている場合スキップ
+      if (substring.match(/^\s|\s$/)) continue;
+
+      // 全てにsubstringが含まれていなければそのsubstringを返す
+      if (strings.every((target) => !target.includes(substring)))
+        return [substring, index];
+    }
+  }
+
+  return [string, 0];
+}
+
+function escapeRegexp(string) {
+  return string.replace(/[.*+\-?^${}()|[\]\\]/g, "\\$&");
+}
+
+function unAt(mention) {
+  return mention.replace(/^[@＠]/, "");
+}
+
+function lowerAll(string) {
+  // 半角スペース、小文字、平仮名、半角記号に変換する
+  return string
+    .toLowerCase() // 小文字
+    .replace(/[ァ-ン]/g, function (s) {
+      return String.fromCharCode(s.charCodeAt(0) - 0x60);
+    })
+    .replace(
+      /[Ａ-Ｚａ-ｚ０-９！＂＃＄％＆＇（）＊＋，－．／：；＜＝＞？＠［＼］＾＿｀｛｜｝]/g,
+      function (s) {
+        return String.fromCharCode(s.charCodeAt(0) - 0xfee0);
+      }
+    )
+    .replace(/[‐－―]/g, "-") // ハイフンなど
+    .replace(/[～〜]/g, "~") // チルダ
+    .replace(/　/g, " "); // スペース
+}
+
 export function searchAndShowResult(memberList, pageSource) {
   const resultMemberList = [];
 
@@ -43,7 +155,7 @@ export function searchAndShowResult(memberList, pageSource) {
     }
   }
 
-  const unAtAndConvertMentionForComparison = function (originalMention) {
+  const convertMentionForComparison = function (originalMention) {
     // @を消して半角スペース、小文字、平仮名、半角記号に変換する
     return originalMention
       .replace(/^[@＠]/, "")
@@ -95,8 +207,7 @@ export function searchAndShowResult(memberList, pageSource) {
     // 短くしないか、もともと1文字
     if (!shorten || unAttedMention.length <= 1) return "@" + unAttedMention;
 
-    const convertedMention =
-      unAtAndConvertMentionForComparison(originalMention);
+    const convertedMention = convertMentionForComparison(originalMention);
 
     const indexesToShortenedMention = new Array(convertedMention.length);
     for (let index = 0; index < convertedMention.length; index++) {
@@ -109,9 +220,7 @@ export function searchAndShowResult(memberList, pageSource) {
     let minLength = 1;
     let minLengthIndex = 0;
     for (const member of resultMemberList) {
-      const convertedLineName = unAtAndConvertMentionForComparison(
-        member["lineName"]
-      );
+      const convertedLineName = convertMentionForComparison(member["lineName"]);
       // それ自身は無視
       if (convertedLineName === convertedMention) continue;
 
--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 import { csvFileToArray, csvUrlToArray } from "./csv-js/csv.js";
 import { fetchPageSource } from "./getPageSource.js";
-import { searchAndShowResult } from "./generateMention.js";
+import { generateMentions } from "./generateMention.js";
+import { showBanner } from "./showBanner.js";
 
 const DefaultInputCsvPath = "member-list.csv";
 
@@ -8,16 +9,29 @@ $(() => {
   $("#input-csv").on("change", () => {
     main();
   });
-  $("form").on("reset", () => {
+  $("#reset-button").on("click", () => {
     $("#input-csv").val("");
     main();
   });
+  $("#shorten-mention-checkbox").on("change", () => {
+    main();
+  });
 
+  $("#shorten-mention-checkbox").prop("checked", true);
   main();
 });
 
 async function main() {
   try {
+    let shortenMentions = $("#shorten-mention-checkbox").prop("checked");
+    if (shortenMentions) {
+      $("#shorten-mention-explanation").show();
+    } else {
+      $("#shorten-mention-explanation").hide();
+    }
+
+    $("#message").text("検索中…");
+
     const csvArray = await (async () => {
       const inputFile = $("#input-csv").prop("files")[0];
       if (inputFile) {
@@ -32,7 +46,52 @@ async function main() {
     })();
     const pageSource = await fetchPageSource();
 
-    searchAndShowResult(csvArray, pageSource);
+    const { mentions, submittedMembers, unSubmittedMembers, regexps } =
+      generateMentions(csvArray, pageSource, shortenMentions);
+
+    $("#message").html(`<h3>未回答者</h3>`);
+    if (!unSubmittedMembers.length) {
+      const $p = $("<p>おめでとうございます！未回答者0人です！</p>");
+      $("#message").append($p);
+    } else {
+      do {
+        const _20mentionString = mentions.splice(0, 20).join(" ");
+        const $p = $(`<p>${_20mentionString}</p>`);
+        $p.click(() => {
+          if (!navigator.clipboard) return;
+
+          navigator.clipboard.writeText(_20mentionString).then(() => {
+            let startString = "";
+            if (mention.length <= 9) {
+              startString = _20mentionString;
+            } else {
+              startString =
+                _20mentionString.substring(0, 6).replace(/\s*@$/, "") + "...";
+            }
+            showBanner(startString + "をコピーしました。");
+          });
+        });
+        $("#message").append($p);
+      } while (mentions.length);
+    }
+
+    $("#number-of-people .count").text(
+      submittedMembers.length + unSubmittedMembers.length
+    );
+    $("#submitted-members .count").text(submittedMembers.length);
+    $("#submitted-members .content").text(
+      submittedMembers
+        .map((member) => `"${member.fullName}"("${member.hitKeyword}")`)
+        .join("、")
+    );
+    $("#un-submitted-members .count").text(unSubmittedMembers.length);
+    $("#un-submitted-members .content").text(
+      unSubmittedMembers.map((member) => `"${member.fullName}"`).join("、")
+    );
+    $("#regexps .count").text(regexps.length);
+    $("#regexps .content").text(
+      regexps.map((regexp) => regexp.toString()).join("、")
+    );
   } catch (e) {
     $("#message").text(`エラーが発生しました: ${e}`);
     throw e;
