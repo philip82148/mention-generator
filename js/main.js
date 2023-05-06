d66c053b2d598234a75d1b4ec0d6a3d2529dec7b
philip82148 <92205270+philip82148@users.noreply.github.com>
2023-05-06 22:27:08 +0900

    tweak

--- js/main.js
+++ js/main.js
@@ -17,86 +17,109 @@ $(() => {
     main();
   });
 
+  $("#control").hide();
   $("#shorten-mention-checkbox").prop("checked", true);
   main();
 });
 
 async function main() {
-  try {
-    $("#control").show();
+  $("#output").text("検索中…");
 
-    let shortenMentions = $("#shorten-mention-checkbox").prop("checked");
-    if (shortenMentions) {
-      $("#shorten-mention-explanation").show();
-    } else {
-      $("#shorten-mention-explanation").hide();
-    }
-
-    $("#output").text("検索中…");
-
-    const csvArray = await (async () => {
-      const inputFile = $("#input-csv").prop("files")[0];
-      if (inputFile) {
+  const csvArray = await (async () => {
+    const inputFile = $("#input-csv").prop("files")[0];
+    if (inputFile) {
+      try {
         $("#selected-file-display").html(`<b>${inputFile.name}</b>`);
-        return csvFileToArray(inputFile);
-      } else {
+        return await csvFileToArray(inputFile);
+      } catch (e) {
+        $("#control").hide();
+        $("#output").text(
+          `エラーが発生しました: メンバーズファイル(${inputFile.name})が読み取れません`
+        );
+        throw e;
+      }
+    } else {
+      try {
         $("#selected-file-display").html(
           `インストールフォルダ内:<b>${DefaultInputCsvPath}</b>`
         );
-        return csvUrlToArray(DefaultInputCsvPath);
+        return await csvUrlToArray(DefaultInputCsvPath);
+      } catch (e) {
+        $("#control").hide();
+        $("#output").text(
+          `エラーが発生しました: デフォルトメンバーズファイル(${DefaultInputCsvPath})が読み取れません`
+        );
+        throw e;
       }
-    })();
-    const pageSource = await fetchPageSource();
-
-    const { mentions, submittedMembers, unSubmittedMembers, regexps } =
-      generateMentions(csvArray, pageSource, shortenMentions);
-
-    $("#output").html(`<h2>未回答者</h2>`);
-    if (!unSubmittedMembers.length) {
-      const $p = $("<p>おめでとうございます！未回答者0人です！</p>");
-      $("#output").append($p);
-    } else {
-      do {
-        const _20mentionString = mentions.splice(0, 20).join(" ");
-        const $p = $(`<p>${_20mentionString}</p>`);
-        $p.click(() => {
-          if (!navigator.clipboard) return;
-
-          navigator.clipboard.writeText(_20mentionString).then(() => {
-            let startString = "";
-            if (_20mentionString.length <= 9) {
-              startString = _20mentionString;
-            } else {
-              startString =
-                _20mentionString.substring(0, 9).replace(/\s*@$/, "") + "...";
-            }
-            showBanner(startString + "をコピーしました。");
-          });
-        });
-        $("#output").append($p);
-      } while (mentions.length);
     }
+  })();
 
-    $("#number-of-people .count").text(
-      submittedMembers.length + unSubmittedMembers.length
-    );
-    $("#submitted-members .count").text(submittedMembers.length);
-    $("#submitted-members .content").text(
-      submittedMembers
-        .map((member) => `"${member.fullName}"("${member.hitKeyword}")`)
-        .join("、")
-    );
-    $("#un-submitted-members .count").text(unSubmittedMembers.length);
-    $("#un-submitted-members .content").text(
-      unSubmittedMembers.map((member) => `"${member.fullName}"`).join("、")
-    );
-    $("#regexps .count").text(regexps.length);
-    $("#regexps .content").text(
-      regexps.map((regexp) => regexp.toString()).join("、")
-    );
-  } catch (e) {
-    $("#control").hide();
-    $("#output").text(`エラーが発生しました: ${e}`);
-    throw e;
+  const pageSource = await (async () => {
+    try {
+      return await fetchPageSource();
+    } catch (e) {
+      $("#control").hide();
+      $("#output").text(`エラーが発生しました: ページソースが読み取れません`);
+      throw e;
+    }
+  })();
+
+  const shortenMentions = $("#shorten-mention-checkbox").prop("checked");
+  if (shortenMentions) {
+    $("#shorten-mention-explanation").show();
+  } else {
+    $("#shorten-mention-explanation").hide();
   }
+
+  const { mentions, submittedMembers, unSubmittedMembers, regexps } =
+    generateMentions(csvArray, pageSource, shortenMentions);
+
+  $("#output").html(`<h2>未回答者</h2>`);
+  if (!unSubmittedMembers.length) {
+    const $p = $("<p>おめでとうございます！未回答者0人です！</p>");
+    $("#output").append($p);
+    $("#control").hide();
+  } else {
+    do {
+      const _20mentionString = mentions.splice(0, 20).join(" ");
+      const $p = $(`<p>${_20mentionString}</p>`);
+      $p.click(() => {
+        if (!navigator.clipboard) return;
+
+        navigator.clipboard.writeText(_20mentionString).then(() => {
+          let startString = "";
+          if (_20mentionString.length <= 9) {
+            startString = _20mentionString;
+          } else {
+            startString =
+              _20mentionString.substring(0, 9).replace(/@$/, "").trim() + "...";
+          }
+          showBanner(startString + "をコピーしました。");
+        });
+      });
+      $("#output").append($p);
+    } while (mentions.length);
+    $("#control").show();
+  }
+
+  $("#number-of-members .count").text(
+    submittedMembers.length + unSubmittedMembers.length
+  );
+
+  $("#submitted-members .count").text(submittedMembers.length);
+  $("#submitted-members .content").text(
+    submittedMembers
+      .map((member) => `"${member.fullName}"("${member.hitKeyword}")`)
+      .join("、")
+  );
+
+  $("#un-submitted-members .count").text(unSubmittedMembers.length);
+  $("#un-submitted-members .content").text(
+    unSubmittedMembers.map((member) => `"${member.fullName}"`).join("、")
+  );
+
+  $("#regexps .count").text(regexps.length);
+  $("#regexps .content").text(
+    regexps.map((regexp) => regexp.toString()).join("、")
+  );
 }
