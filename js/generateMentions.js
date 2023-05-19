f0e6dbd2dad1b98a2dc823ce1f4f9400b3bda493
philip82148 <92205270+philip82148@users.noreply.github.com>
2023-05-19 17:20:42 +0900

    refactoring

--- js/generateMentions.js
+++ js/generateMentions.js
@@ -1,21 +1,21 @@
 import { shortenMentions } from "./shortenMentions.js";
 
-export function generateMentions(members, pageSource, shorten) {
+export function generateMentions(csvArray, pageSource, shorten) {
   const submittedMembers = [];
   const unSubmittedMembers = [];
   const lineNames = [];
   const regexps = [];
-  for (const member of members) {
-    const [resultMember, regexpsPerMember] = searchForMember(
-      member,
+  for (const csvRow of csvArray) {
+    const { regexps: regexpsPerMember, ...member } = searchForMember(
+      csvRow,
       pageSource
     );
-    if (resultMember.hitKeyword) {
-      submittedMembers.push(resultMember);
+    if (member.hitKeyword) {
+      submittedMembers.push(member);
     } else {
-      unSubmittedMembers.push(resultMember);
+      unSubmittedMembers.push(member);
     }
-    lineNames.push(resultMember.lineName);
+    lineNames.push(member.lineName);
     regexps.push(...regexpsPerMember);
   }
 
@@ -25,17 +25,19 @@ export function generateMentions(members, pageSource, shorten) {
   return { mentions, submittedMembers, unSubmittedMembers, regexps };
 }
 
-function searchForMember(member, pageSource) {
-  const fullName = member[0].trim();
-  const lineName = member[member.length - 1].trim().replace(/^[@＠]/, "");
+function searchForMember(csvRow, pageSource) {
+  const fullName = String(csvRow[0]).trim();
+  const lineName = String(csvRow[csvRow.length - 1])
+    .trim()
+    .replace(/^[@＠]/, "");
 
   const regexps = [];
   for (let i = 0; ; i++) {
-    const searchKeyword = member[i].trim();
+    const searchKeyword = String(csvRow[i]).trim();
     if (!searchKeyword) continue;
 
     // lineNameは検索しない。但し要素数が1個しかないときは別
-    if (i >= member.length - 1 && member.length > 1) break;
+    if (i >= csvRow.length - 1 && csvRow.length > 1) break;
 
     // 空白は空白を任意の数含む文字として検索
     const regexp = new RegExp(
@@ -46,10 +48,10 @@ function searchForMember(member, pageSource) {
 
     // 検索
     if (pageSource.match(regexp))
-      return [{ fullName, lineName, hitKeyword: searchKeyword }, regexps];
+      return { regexps, fullName, lineName, hitKeyword: searchKeyword };
   }
 
-  return [{ fullName, lineName }, regexps];
+  return { regexps, fullName, lineName };
 }
 
 function escapeRegexp(string) {
